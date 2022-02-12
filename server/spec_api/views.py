26482d83a4b7fdebc9d9731913cb9bc8208a0927
mitsu-h <u.world96@gmail.com>
2022-02-12 18:43:00 +0900

    修正、HelloWorld削除

--- server/spec_api/views.py
+++ server/spec_api/views.py
@@ -1,37 +1,29 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from rest_framework import status
 import librosa
-import io
-
-# Create your views here.
-class HelloWorld(APIView):
-    """
-    クラスベースのAPIView
-    ・HTTPのメソッドがクラスのメソッドになるので、わかりやすい
-    """
-
-    def get(self, request, format=None):
-        return Response({"message": "Hello World!!"}, status=status.HTTP_200_OK)
-
-    def post(self, request, format=None):
-        request_data = request.data
-        return Response(
-            {"message": request_data["message"]}, status=status.HTTP_201_CREATED
-        )
+import tempfile
 
 
 class GetWav(APIView):
-    """
-    """
-
     def get(self, request, format=None):
         return Response({"message": "音声ファイルをPOSTして下さい"}, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
+        """音声ファイルから、波形のデータとサンプリングレートを獲得する
+
+        Args:
+            request (dict[_io.BufferedReader]): ファイルオブジェクト
+            format ([type],  optional): [description]. Defaults to None.
+
+        Returns:
+            [dict[numpy.ndarray, int]]: 波形の配列と、サンプリングレート
+        """
         request_data = request.data
-        wav, fs = librosa.load(io.BytesIO(request_data["file"].read()), sr=None)
+        fp = tempfile.NamedTemporaryFile()
+        fp.write(request_data["file"].read())
+        fp.seek(0)
+        # wav, fs = librosa.load(io.BytesIO(request_data["file"].read()), sr=None)
+        wav, fs = librosa.load(fp.name, sr=None)
         return Response({"wav": wav, "fs": fs}, status=status.HTTP_200_OK)
